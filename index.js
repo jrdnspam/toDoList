83af91cf80c279f4b0924e532be4a779ceb1b6de
jwopshit <131551791+jwopshit@users.noreply.github.com>
2023-09-27 12:54:20 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function() {
+  // Code placed here will run when the DOM content has loaded.
+  // Returns the first element with the 'todo-input' id
+document.getElementById('todo-input');
+
+});
